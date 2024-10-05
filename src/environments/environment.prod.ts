750fcbccad00100d09851234ea1f51bf7e84d212
wassim <wassimrached404@gmail.com>
2024-10-05 19:48:21 +0100

    update env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ import packageInfo from '../../package.json';
 
 export const environment = {
   appVersion: packageInfo.version,
-  production: true
+  production: true,
+  cas: 'https://cas-server.azurewebsites.net',
+  ics: 'https://ics-server.azurewebsites.net'
 };
