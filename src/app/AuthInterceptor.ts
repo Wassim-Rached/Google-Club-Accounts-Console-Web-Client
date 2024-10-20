cf57a42eee0502b92fd0926e8abc4ade8cfe4b13
wassim <wassimrached404@gmail.com>
2024-10-20 11:21:03 +0100

    Refactor AuthInterceptor to fix toastr error message for insufficient permission

--- src/app/AuthInterceptor.ts
+++ src/app/AuthInterceptor.ts
@@ -1,15 +1,13 @@
 import { inject } from '@angular/core';
 import { HttpRequest, HttpEvent, HttpInterceptorFn, HttpHandlerFn } from '@angular/common/http';
 import { catchError, Observable, throwError } from 'rxjs';
+
 import { ToastrService } from 'ngx-toastr';
-import { AuthService } from './services/auth/auth.service';
-import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
+import { AuthService } from './services/auth/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (request: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> => {
   const authService = inject(AuthService);
-  const toastrService = inject(ToastrService);
-  const router = inject(Router);
 
   const token = authService.getToken();
   if (token) {
@@ -20,21 +18,23 @@ export const authInterceptor: HttpInterceptorFn = (request: HttpRequest<any>, ne
     });
   }
 
+  const toastrService = inject(ToastrService);
+
   return next(request).pipe(
     catchError((error) => {
       // Check if the error status is 403
       switch (error.status) {
         case 403:
-          // check if the request is not the login request
-          if (request.url !== `${environment.cas}/api/token`) {
+          if (authService.isAuthenticated()) {
             toastrService.error('Insufficent permission');
           }
+          // Optionally redirect to login or another page
+          // router.navigate(['/login']); // Uncomment this line if you want to redirect
           break;
         case 401:
-          // check if the request is not the login request
-          if (request.url !== `${environment.cas}/api/token`) {
-            toastrService.error('Unauthorized');
-          }
+          toastrService.error('Unauthorized');
+          // Optionally redirect to login or another page
+          // router.navigate(['/login']); // Uncomment this line if you want to redirect
           break;
         case 404:
           toastrService.error('Resource not found');
@@ -42,8 +42,11 @@ export const authInterceptor: HttpInterceptorFn = (request: HttpRequest<any>, ne
         case 500:
           toastrService.error('Something went wrong');
           break;
+        case 0:
+          const serverName = getServerNameByUrl(error.url);
+          toastrService.warning(serverName + ' server might be down');
+          break;
         default:
-          toastrService.warning('Servers might be down');
           break;
       }
       // Rethrow the error to be handled by other parts of the app
@@ -51,3 +54,13 @@ export const authInterceptor: HttpInterceptorFn = (request: HttpRequest<any>, ne
     })
   );
 };
+
+function getServerNameByUrl(url: string): string {
+  if (url.startsWith(environment.cas)) {
+    return 'CAS';
+  }
+  if (url.startsWith(environment.ics)) {
+    return 'ICS';
+  }
+  return 'Unknown';
+}
