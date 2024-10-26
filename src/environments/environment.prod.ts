b290736fd1dff41eea528ab7fa9c9e3b825d8bb3
wassim <wassimrached404@gmail.com>
2024-10-26 17:16:45 +0100

    Refactor environment.prod.ts to include messageDisplayDurationInMs and authorities

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,11 +4,16 @@ export const environment = {
   appVersion: packageInfo.version,
   production: true,
   defaultPhotoUrl: 'assets/images/user/avatar-2.jpg',
+  messageDisplayDurationInMs: 20000,
 
   // central authentication service
   cas: 'https://cas-server.azurewebsites.net',
   // identity and access control service
   ics: 'https://ics-server.azurewebsites.net',
   // account management web client
-  amwc: 'https://account-manager-eta.vercel.app'
+  amwc: 'https://account-manager-eta.vercel.app',
+
+  authorities: {
+    access_client: 'acwc.perm.access_client'
+  }
 };
